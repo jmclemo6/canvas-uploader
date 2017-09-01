541c5864c232ab009d7397957248e8bcfb1c3877
Jacob McLemore <jmclemo6@vols.utk.edu>
2017-09-01 18:15:43 -0400

    Add some features

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 from os.path import isfile
 
 import requests
@@ -11,45 +12,75 @@ except ImportError:
 
 # Create module-wide authenticated session
 auth_session = requests.Session()
-auth_session.headers = {'Authorization': 'Bearer {0}'.format(config.auth_token)}
+auth_session.headers = {
+    'Authorization': 'Bearer {0}'.format(
+        config.auth_token)}
 
 
 def get_course():
     try:
-        response = auth_session.get(''.join([config.domain, '/api/v1/courses']))
+        response = auth_session.get(
+            ''.join([config.domain, '/api/v1/courses']))
     finally:
         auth_session.close()
-
+    
+    latestTerm = 0
     for i in range(len(response.json())):
-        print("[{0}] {1}".format(i + 1, response.json()[i]['name']))
+        if 'enrollment_term_id' in response.json()[i] and response.json()[i]['enrollment_term_id'] > latestTerm:
+            latestTerm = response.json()[i]['enrollment_term_id']
+
+    courses = list(
+        filter(
+            lambda a: 'enrollment_term_id' in a and a['enrollment_term_id'] >= latestTerm and 'name' in a,
+            response.json()))
+
+    for i in range(len(courses)):
+        print("[{0}] {1}".format(i + 1, courses[i]['name']))
 
     selection = int(input("Select Course: ")) - 1
 
-    return response.json()[selection]['id']
+    return courses[selection]['id']
 
 
 def get_assignment(class_id):
-    url = ''.join([config.domain, '/api/v1/courses/{0}/assignments'.format(class_id)])
-    params = {'bucket': 'upcoming'}
+    url = ''.join(
+        [config.domain, '/api/v1/courses/{0}/assignments'.format(class_id)])
+    params = {'include': ['submission']}
     try:
         response = auth_session.get(url, params=params)
     finally:
         auth_session.close()
-
-    assignments = list(filter(lambda a: 'online_upload' in a['submission_types'], response.json()))
+    assignments = list(
+        filter(
+            lambda a: 'online_upload' in a['submission_types'] and a['locked_for_user'] != True,
+            response.json()))
 
     print()
+    if not assignments:
+        print("This class has no assignments aviliable for you.")
+        return None
     for i in range(len(assignments)):
         print("[{0}] {1}".format(i + 1, assignments[i]['name']))
 
     selection = int(input("Select assignment: ")) - 1
+    
+    if 'submission' in assignments[selection]:
+        choice = ""
+        while choice != "n" and choice != "y":
+            choice = input("This assignment already has a submisson. Continue? [y/n]: ")
+        if choice.lower() == "n":
+            return None
 
     return assignments[selection]['id']
 
 
 def get_file():
-    files = list(filter(isfile, os.listdir('.')))
+    cwd = os.getcwd()
+    files = list(filter(isfile, os.listdir(cwd)))
 
+    if not files:
+        print("No files to upload in current directory.")
+        return None;
     print()
     for i in range(len(files)):
         print("[{0}] {1}".format(i + 1, files[i]))
--- main.py
+++ main.py
@@ -10,11 +10,16 @@ if __name__ == '__main__':
     try:
         class_id = UserInterface.get_course()
         assign_id = UserInterface.get_assignment(class_id)
+        if not assign_id:
+            sys.exit()
         file_name = UserInterface.get_file()
-
+        if not file_name:
+            sys.exit()
         UploadFile.upload_file(class_id, assign_id, file_name)
     except HTTPError:
-        print("Canvas returned an error. Please try again later.", file=sys.stderr)
+        print(
+            "Canvas returned an error. Please try again later.",
+            file=sys.stderr)
         sys.exit()
 
     print("\nAssignment submitted.")
